ee2b2d26f965248367893d2d59f27c80a6eb16cf
Avishka Chasith Gunathilaka <chasithgunathilaka@gmail.com>
2026-01-07 10:19:53 +0530

    Basic login completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { DashboardShell } from "@/app/dashboard/components/dashboard-shell"
-
+import { DashboardShell } from "@/app/dashboard/components/dashboard-shell";
+//import LoginPage from "@/app/login/page";
 export default function Home() {
-  return <DashboardShell />
+  return <DashboardShell />;
 }
