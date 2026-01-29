a3ad066e55084e25c219e9aeea7f35079ec02d69
Avishka Chasith <chasithgunathilaka@gmail.com>
2026-01-29 12:27:54 +0530

    add table view into the card content

--- src/app/(root)/developers/page.tsx
+++ src/app/(root)/developers/page.tsx
@@ -44,6 +44,45 @@ export default function DevelopersPage() {
                         Columns  <ChevronDown className="ml-2 h-4 w-4" />
                     </Button>
                 </CardHeader>
+
+                <CardContent>
+                    <div className="rounded-md border">
+                        <Table>
+                            <TableHeader>
+                                <TableRow className="bg-muted/50">
+                                <TableHead className="w-[50px]"><input type="checkbox" />
+                                </TableHead>
+                                <TableHead>Name</TableHead>
+                                <TableHead className="cursor-pointer hover:text-primary">Jira <ArrowUpDown className="inline ml-2 h-3 w-3"/>
+                                </TableHead>
+                                <TableHead>GitHub</TableHead>
+                                <TableHead>Role</TableHead>
+                                <TableHead className="w-[50px]"></TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {
+                                    developers.map((dev)=>(
+                                        <TableRow key = {dev.id} className="hover:bg-muted/30">
+                                            <TableCell>
+                                                <input type="checkbox" />
+                                            </TableCell>
+                                            <TableCell className="font-medium">{dev.name}</TableCell>
+                                            <TableCell>{dev.jiraEmail}</TableCell>
+                                            <TableCell>{dev.githubEmail}</TableCell>
+                                            <TableCell>{dev.role}</TableCell>
+                                            <TableCell>
+                                                <Button variant="ghost" size="icon" className="h-8 w-8">
+                                                    <MoreHorizontal className="h-4 w-4"/>
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))
+                                }
+                            </TableBody>
+                        </Table>
+                    </div>
+                </CardContent>
             </Card>
         </div>
     )
