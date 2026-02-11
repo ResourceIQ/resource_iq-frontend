aef08091be26bc3f023ad9750de7523f7a5714af
Diluka Lahiru <106172155+dilukalahiru@users.noreply.github.com>
2026-02-11 14:27:36 +0530

    feat: Enhance StatCard component to display an icon and an additional message, and update dashboard cards to utilize these new properties.

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -1,15 +1,16 @@
 import { StatCard } from "@/components/StatCard"
+import { Users ,Clock,TrendingUp,CircleCheckBig} from 'lucide-react';
 
 
 export default function DashboardPage() {
     return (
-        <div className="p-6">{/* This is the root container */}
+        <div className="p-6">
             
             <div className="grid grid-cols-4 gap-4 mb-8">
-                <StatCard title="Team Members" value="6" />
-                <StatCard title="Team Utilization" value="59%" />
-                <StatCard title="Active Tasks" value="9" />
-                <StatCard title="Pending Assignments" value="5" />
+                <StatCard title="Team Members" value="6" massage="+2 this month" Iconname={Users}/>
+                <StatCard title="Team Utilization" value="59%" massage="Monitor closely"Iconname={TrendingUp}/>
+                <StatCard title="Active Tasks" value="9" massage="12 completed this week"Iconname={CircleCheckBig}/>
+                <StatCard title="Pending Assignments" value="5" massage="Needs attention"Iconname={Clock}/>
             </div>
 
             
--- src/components/StatCard.tsx
+++ src/components/StatCard.tsx
@@ -1,8 +1,25 @@
-export function StatCard({ title, value }: { title: string; value: string }) {
+import { LucideIcon } from "lucide-react";
+
+export function StatCard({ 
+  title, 
+  value, 
+  massage, 
+  Iconname 
+}: { 
+  title: string; 
+  value: string; 
+  massage: string; 
+  Iconname: LucideIcon; 
+}) {
   return (
     <div className="border border-black p-6 rounded-lg">
       <p className="text-xs font-bold uppercase">{title}</p>
       <h2 className="text-3xl font-bold mt-2">{value}</h2>
+
+      <div className="px-1 gap-2 flex items-start mx mt-4">
+        <Iconname className="h-5 w-5 text-gray-600" />
+        <p>{massage}</p>
+      </div>
     </div>
   );
 }
